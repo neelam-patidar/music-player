436f726e1cf581482dfca149018b158fe50eee46
neelam-patidar <neelampatidar235@gmail.com>
2023-07-28 21:38:17 +0530

    music player with html css js

---
+++ data.js
@@ -0,0 +1,33 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'musics/falak.mp3',
+        artist: 'artist 1',
+        cover : 'images/img1.jpg',
+    },
+    {
+        name: 'song 2',
+        path: 'musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover : 'images/img2.jpg',
+    },
+    {
+       name: 'song 3',
+        path: 'musics/Song 3.mp3',
+        artist: 'artist 3',
+        cover : 'images/img3.jpg',
+    },
+    {
+       name: 'song 4',
+        path: 'musics/Song 4.mp3',
+        artist: 'artist 4',
+        cover : 'images/img4.jpg',
+    },
+    {
+        name: 'song 5',
+        path: 'musics/Song 5.mp3',
+        artist: 'artist 5',
+        cover : 'images/img5.jpg',
+    },
+
+]
